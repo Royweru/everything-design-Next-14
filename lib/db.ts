1768fb1a9cd61e90d48fa13c21b2cb051b851ff7
Royweru <royezi17@gmail.com>
2024-09-15 18:49:15 +0300

    modified:   lib/db.ts 	modified:   package-lock.json 	modified:   package.json

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
+
+
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+const client = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV === "development") {
   if (!globalThis.prisma) {
